1f3e76149822a93de9758157735c8cb103856349
Bobby Kuepper <robertkuepper@gmail.com>
2023-10-15 22:27:07 -0500

    test

--- test/indexTest.js
+++ test/indexTest.js
@@ -49,9 +49,6 @@ describe('basic math functions', function () {
   })
 })
 
-
-
-
 describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
     expect(makeInt(a.toString())).toEqual(a)
